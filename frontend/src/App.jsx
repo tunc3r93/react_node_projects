66bbc759c2faddc7c239a6a1974a8772394842c2
tuncer <tuncer.dev@mail.de>
2025-04-05 23:46:15 +0200

    feat: create Navbar component and integrate it into App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import LoginPage from './pages/Auth/LoginPage';
+import Navbar from './components//Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <LoginPage />
     </>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <button className="logout-btn" disabled>Ausloggen</button>
+        </nav>
+    );
+};
+
+export default Navbar;
